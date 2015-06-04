3c32152058e39f7be5c2ce687d272a0065b8ae65
Tim Clem <tim.clem@gmail.com>
2015-06-04 09:23:20 -0400

    Initial Commit

---
+++ ios-collections-and-strings-problems/Resources/Other-Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ios-collections-and-strings-problems
+//
+//  Created by iOS Staff on 6/4/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FISAppDelegate class]));
+    }
+}
